247935f6bc77b88f20efeefe452248ccb93fea5b
dmtuan2 <0mtmtmt@gmail.com>
2023-11-08 11:10:45 +0700

    initialization commit

---
+++ src/main/java/com/example/springargular/SpringArgularApplication.java
@@ -0,0 +1,14 @@
+package com.example.springargular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class SpringArgularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringArgularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springargular/SpringArgularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springargular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArgularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
